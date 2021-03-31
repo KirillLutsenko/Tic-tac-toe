68f112983059bb3408e0dbaf4344714161968d45
KirillLutsenko <mnrovski68@gmail.com>
2021-03-31 11:29:53 +0300

    fixes types

--- src/Types.js
+++ src/Types.js
@@ -1,17 +1,5 @@
 import PropTypes from 'prop-types';
 
-// export const PostType = PropTypes.shape({
-//   id: PropTypes.number.isRequired,
-//   title: PropTypes.string.isRequired,
-//   userId: PropTypes.number.isRequired,
-// }).isRequired;
-
-// export const PostListType = {
-//   posts: PropTypes.arrayOf(PostType),
-//   onPostSelected: PropTypes.func.isRequired,
-//   selectedPostId: PropTypes.number.isRequired,
-// };
-
 export const SideSwitchType = {
   firstPlayerX: PropTypes.bool.isRequired,
   setFirstPlayerX: PropTypes.func.isRequired,
